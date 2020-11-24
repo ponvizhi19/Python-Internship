f54193e41ee7d76c3d812eacb1bae5cd08026c8a
ponvizhi19 <74911744+ponvizhi19@users.noreply.github.com>
2020-11-24 11:31:03 +0530

    Create Day2.py

---
+++ Day2.py
@@ -0,0 +1,20 @@
+#1)Create three variables (a,b,c) to same value of any integer & do the following
+#1.Divide a by 10
+#2.Multiply b by 50
+#3.Add c value by 60
+a=b=c=5
+Print(a/10)
+Print(b*50)
+Print(c+60)
+
+#2)Create a String variable of 5 characters  and replace the 3rd character with G
+s='kayal'
+t=s.replace('y','g')
+Print(t)
+
+#3)Create two values (a,b) of int,float data type & convert the vise versa, Hint : convert a from int to float datatype & b from float to int datatype 
+(a,b)=(18,26.7)
+Print(int(a))
+Print(float(a))
+Print(flaot(b))
+Print(int(b))
