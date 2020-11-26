ed0d927e4b571f4cb31b2f02391858286b51da7e
ponvizhi19 <74911744+ponvizhi19@users.noreply.github.com>
2020-11-26 19:25:13 +0530

    Create Day4.py

---
+++ Day4.py
@@ -0,0 +1,41 @@
+#Write a program to create a list of n integer values and do the following
+#Add an item in to the list (using function)
+a=[1,2,3,4]
+a.append(5)
+print(a)
+
+#Delete (using function)
+a=[1,2,3,4]
+a.remove(2)
+print(a)
+
+#Store the largest number from the list to a variable
+a=[1,4,6,2]
+b=max(a)
+print(b)
+
+#Store the Smallest number from the list to a variable
+a=[1,4,6,2]
+b=min(a)
+print(b)
+
+#2) Create a tuple and print the reverse of the created tuple
+a=(1,4,6,2)
+b=reversed(a)
+print(tuple(b))
+
+#3) Create a tuple and convert tuple into list
+a=(1,4,6,2)
+b=list(a)
+print(b)
+
+
+
+
+
+
+
+
+
+
+
