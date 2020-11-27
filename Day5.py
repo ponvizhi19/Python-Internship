11f0f1eb2a8cf6bd0c7cf4cb41f9114442685f91
ponvizhi19 <74911744+ponvizhi19@users.noreply.github.com>
2020-11-27 18:57:57 +0530

    Create Day5.py

---
+++ Day5.py
@@ -0,0 +1,50 @@
+# 1)Create a function getting two integer inputs from user. & print the following:
+
+#Addition of two numbers is +value
+x=int(input("enter the first number:"))
+y=int(input("enter the second number:"))
+def add(a,b):
+ print ("the sum is",+a+b)
+add(x,y)
+
+#Subtraction of two numbers is +value
+x=int(input("enter the first number:"))
+y=int(input("enter the second number:"))
+def diff(a,b):
+ print ("the sum is",+a-b)
+diff(x,y)
+
+#Division of two numbers is +value
+x=int(input("enter the first number:"))
+y=int(input("enter the second number:"))
+def div(a,b):
+ print ("the sum is",+a/b)
+div(x,y)
+
+#Multiplication of two numbers is +value
+a=int(input("enter the first number:"))
+b=int(input("enter the second number:"))
+def div(x,y):
+ print ("the sum is",+x*y)
+div(a, b)
+
+#2. Create a function covid( ) & it should accept patient name, and body temperature, by default the body temperature should be 98 degree
+a=input()
+
+b='98°C'
+
+def covid(x,y):
+
+ print ("Name of the patient =",x)
+
+ print("the body temperature =",y)
+
+covid(a, b)
+
+
+
+
+
+
+ 
+
