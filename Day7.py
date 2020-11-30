f13fcc6ec82b33342437ecac005eb72a3abda655
ponvizhi19 <74911744+ponvizhi19@users.noreply.github.com>
2020-11-30 21:26:50 +0530

    Create Day7.py

---
+++ Day7.py
@@ -0,0 +1,23 @@
+# Python-Internship
+#1
+From IMP Import mylist
+mylist()
+
+#2
+import pandas as pd
+a=[15,24,45,50,75,100,8]
+L=pd.Series(a)
+print(L)
+
+#3
+import random
+print("Printing random number using random.random()")
+print(random.random())
+
+#4
+Import sys
+sys.path
+
+#5
+pip install pandas  
+pip uninstall pandas
